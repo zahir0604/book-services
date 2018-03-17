9eec05ac34b30fb05947f5904178ba94df9a7517
Mohamed.Zahirudeen <Mohamed.Zahirudeen@wolterskluwer.com>
2018-03-17 11:25:20 -0400

    commit

--- src/main/java/twyla/comments/CommentsByUser.java
+++ src/main/java/twyla/comments/CommentsByUser.java
@@ -5,14 +5,6 @@ public class CommentsByUser extends Comment {
 
     private String title;
 
-    public CommentsByUser() {
-    }
-
-    public CommentsByUser(String bookId, String comment, String rating, String user, String title) {
-        super(bookId, comment, rating, user);
-        this.title = title;
-    }
-
     public String getTitle() {
 
         return title;
